84ab24e6156aa9d8ed13c37f7cd9143c1e60128b
antonvh <antonhaninvl@gmail.com>
2019-10-11 17:13:51 +0300

    refactoring: remove invalide code

--- src/linked-list.js
+++ src/linked-list.js
@@ -31,25 +31,11 @@ class LinkedList {
     }
 
     at(index) {
-        var currentNode = this._head.data,       
-        length = this.length,
-        count = 0,
-        message = { failure: 'Failure: non-existent node in this list.' };
-        // 1-ый случай: неверная позиция 
-        if (length === 0 || index < 1 || index > length) {
-            throw new Error(message.failure);
-        }
-
-        // 2-ой случай: верная позиция 
-        while (count < index) {
-            currentNode = currentNode.next.data;
-            count++;
-        }
-        return currentNode.data;
 
     }
 
-    insertAt(index, data) {}
+    insertAt(index, data) {
+    }
 
     isEmpty() {
         return (this._head == null) ? true : false;
@@ -62,56 +48,6 @@ class LinkedList {
     }
 
     deleteAt(index) {
-        var currentNode = this.head,
-       length = this._length,
-       count = 1,
-       message = { failure: 'Failure: non-existent node in this list.' },
-       beforeNodeToDelete = null,
-       nodeToDelete = null,
-       deletedNode = null;
-
-        // 1-ый случай: неверная позиция
-        if (length === 0 || index < 1 || index > length) {
-            throw new Error(message.failure);
-        }
-
-        // 2-ой случай: первый узел удален
-        if (index === 1) {
-            this.head = currentNode.next;
-
-            // 2-ой случай: существует второй узел
-            if (!this.head) {
-                this.head.previous = null;
-                // 2-ой случай: второго узла не существует
-            } else {
-                this.tail = null;
-            }
-
-            // 3-ий случай: последний узел удален
-        } else
-            if (index === this._length) {
-                this.tail = this.tail.previous;
-                this.tail.next = null;
-            // 4-ый случай: средний узел удален
-            } else {
-                while (count < index) {
-                    currentNode = currentNode.next;
-                    count++;
-                }
-
-                beforeNodeToDelete = currentNode.prev;
-                nodeToDelete = currentNode;
-                afterNodeToDelete = currentNode.next;
-
-                beforeNodeToDelete.next = afterNodeToDelete;
-                afterNodeToDelete.prev = beforeNodeToDelete;
-                deletedNode = nodeToDelete;
-                nodeToDelete = null;
-            }
-
-        this._length--;
-
-        return message.success;
     }
 
     reverse() {}
@@ -119,7 +55,6 @@ class LinkedList {
     indexOf(data) {
         var currNode = this._head,
         count = 0;
-        console.log(currNode, 'why', data);
         if (currNode.data != null && data != null) {
             while (currNode.data != data) {
                 currNode = currNode.next;
